4e433935261e1d54fc71de9fa74f4552b5c224ac
Bas Nijholt <basnijholt@gmail.com>
2016-07-07 13:42:49 +0200

    Bump version number to 1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hpc05',
-      version='1.0',
+      version='1.1',
       description='Client package for local TU Delft cluster',
       url='https://gitlab.kwant-project.org/cwg/hpc05',
       author='Michael Wimmer and Bas Nijholt',
