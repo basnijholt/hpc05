050fbb34013b6ec50c2c8f49f189161cf01119b9
Bas Nijholt <basnijholt@gmail.com>
2016-09-20 18:22:16 +0200

    Correct url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='hpc05',
       version='1.1',
       description='Client package for local TU Delft cluster',
-      url='https://gitlab.kwant-project.org/cwg/hpc05',
+      url='https://github.com/basnijholt/hpc05',
       author='Michael Wimmer and Bas Nijholt',
       license='BSD 2-clause',
       py_modules=["hpc05", "hpc05_culler"],
