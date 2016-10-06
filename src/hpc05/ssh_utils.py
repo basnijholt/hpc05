6b3fc019ab162f2ce952fc7bfe65e637104898db
Bas Nijholt <basnijholt@gmail.com>
2016-10-06 14:43:46 +0200

    small style change

--- src/hpc05/ssh_utils.py
+++ src/hpc05/ssh_utils.py
@@ -35,8 +35,8 @@ def setup_ssh(hostname='hpc05', username=None, password=None):
 
 
 def check_bash_profile(ssh, username):
-    # Check if there is a .bash_profile remotely, sometimes the PYTHON_PATH
-    # exported in the .bash_profile instead of .bash_rc
+    """Check if there is a .bash_profile remotely. In some cases the PYTHON_PATH
+    is exported in the `.bash_profile` instead of `.bash_rc`."""
     with ssh.open_sftp() as sftp:
         try:
             fname = '/home/{}/.bash_profile'.format(username)
