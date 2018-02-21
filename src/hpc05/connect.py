9164522d4ca032b1654a68bc7fade291684b3654
Bas Nijholt <basnijholt@gmail.com>
2018-02-21 21:27:31 +0100

    make sure that None is not passed as a str

--- src/hpc05/connect.py
+++ src/hpc05/connect.py
@@ -87,6 +87,9 @@ def start_ipcluster(n, profile, env_path=None, timeout=60):
 def start_remote_ipcluster(n, profile='pbs', hostname='hpc05',
                            username=None, password=None, env_path=None, 
                            timeout=60):
+    if env_path is None:
+        env_path = ''
+
     with setup_ssh(hostname, username, password) as ssh:
         cmd = f"import hpc05; hpc05.start_ipcluster({n}, '{profile}', '{env_path}', {timeout})"
         cmd = f'python -c "{cmd}"'
