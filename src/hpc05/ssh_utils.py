6a1e34b76440ea7c106fa7c0c7805b1b5c32f35f
Bas Nijholt <basnijholt@gmail.com>
2018-09-06 17:24:05 +0200

    allow sshing with a proxy

--- src/hpc05/ssh_utils.py
+++ src/hpc05/ssh_utils.py
@@ -1,6 +1,6 @@
 import os.path
 import paramiko
-from paramiko.ssh_exception import PasswordRequiredException
+from paramiko.ssh_exception import PasswordRequiredException, SSHException
 
 
 def get_info_from_ssh_config(hostname):
@@ -12,22 +12,23 @@ def get_info_from_ssh_config(hostname):
     cfg = ssh_config.lookup(hostname)
     full_hostname = cfg['hostname']
     username = cfg['user']
-    return username, full_hostname
+    proxy = (paramiko.ProxyCommand(cfg['proxycommand'])
+             if 'proxycommand' in cfg else None)
+    return username, full_hostname, proxy
 
 
 def setup_ssh(hostname='hpc05', username=None, password=None):
     if username is None:
         try:
-            username, hostname = get_info_from_ssh_config(hostname)
+            username, hostname, proxy = get_info_from_ssh_config(hostname)
         except KeyError:
             raise Exception('hostname not in ~/.ssh/config, enter username')
-
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
         ssh.connect(hostname, username=username, allow_agent=True,
-                    password=password)
-    except PasswordRequiredException:
+                    password=password, sock=proxy)
+    except (PasswordRequiredException, SSHException):
         msg = ['Enter `password` argument or run',
                'rm -f ~/ssh-agent.socket; ssh-agent -a ~/ssh-agent.socket; '
                'export SSH_AUTH_SOCK=~/ssh-agent.socket; ssh-add',
