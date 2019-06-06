81694c70027abbab64a92cbf0cf235344982859c
Bas Nijholt <basnijholt@gmail.com>
2019-06-06 11:44:50 -0700

    add long_description

--- setup.py
+++ setup.py
@@ -17,11 +17,30 @@ def get_version_and_cmdclass(package_name):
 version, cmdclass = get_version_and_cmdclass("hpc05")
 
 
+def text_between_markers(fname, between):
+    with open(fname) as f:
+        lines = f.readlines()
+    do_append = False
+    long_description = []
+    for line in lines:
+        if any(marker in line for marker in between):
+            do_append = not do_append
+            continue
+        if do_append:
+            long_description.append(line)
+    return "".join(long_description).strip("\n")
+
+
+long_description = text_between_markers(
+    fname="README.md", between=["# hpc05", "# Installation"]
+)
+
 setup(
     name="hpc05",
     version=version,
     cmdclass=cmdclass,
     description="Client package for PBS and SLURM clusters with a headnode.",
+    long_description=long_description,
     url="https://github.com/basnijholt/hpc05",
     author="Bas Nijholt",
     author_email="basnijholt@gmail.com",
